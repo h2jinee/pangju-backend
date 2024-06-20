6126534d607d30ea837d282d741c3644dde5f72b
HEEJIN JEON <wjsgmlwls97@gmail.com>
2024-06-21 00:52:26 +0900

    feat/게시글 상세페이지 임시 API 추가

--- pangju/src/main/java/com/pangju/pangju/domain/Item.java
+++ pangju/src/main/java/com/pangju/pangju/domain/Item.java
@@ -5,16 +5,18 @@ import lombok.Setter;
 
 @Getter @Setter
 public class Item {
+    private int id;
     private String mainCategory;
     private String subCategory;
-    private String status;
-    private String content;
-    private String description;
+    private String status; // 상태 (찾는 중, 찾기 완료)
+    private String content; // 제목
+    private String description; // 내용
     private String image;
     private int heartCount;
     private int chatCount;
 
-    public Item(String mainCategory, String subCategory, String status, String content, String description, String image, int heartCount, int chatCount) {
+    public Item(int id, String mainCategory, String subCategory, String status, String content, String description, String image, int heartCount, int chatCount) {
+        this.id = id;
         this.mainCategory = mainCategory;
         this.subCategory = subCategory;
         this.status = status;
